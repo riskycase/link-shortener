ec2ed68bed8761be565b8da1c41ba9ded85478b0
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2024-07-28 20:02:24 +0530

    Disable standalone mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "Link Shortener",
-      script: "./.next/standalone/server.js",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
       env: {
         PORT: 5132,
         NEXTAUTH_URL: "https://short.riskycase.in",
